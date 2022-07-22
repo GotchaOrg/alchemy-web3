4d08f07eb482fb6c5088a0834aabb5b1449b4950
Joaquin Gonzalez <jpgonzalezra@gmail.com>
2022-07-22 14:10:55 -0300

    add dist folder (test)

---
+++ dist/esm/web3-adapter/alchemySendHttp.d.ts
@@ -0,0 +1,4 @@
+import { AlchemySendJsonRpcFunction } from "./alchemySend";
+export declare function makeJsonRpcHttpSender(
+  url: string,
+): AlchemySendJsonRpcFunction;
---
+++ dist/esm/web3-adapter/alchemySendWebSocket.d.ts
@@ -0,0 +1,5 @@
+import SturdyWebSocket from "sturdy-websocket";
+import { AlchemySendJsonRpcFunction } from "./alchemySend";
+export declare function makeWebSocketSender(
+  ws: SturdyWebSocket,
+): AlchemySendJsonRpcFunction;
